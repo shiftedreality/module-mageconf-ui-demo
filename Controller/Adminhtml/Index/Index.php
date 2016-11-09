6f0114c981a774579437ecd9c4897d35f8a62d5b
Oleh Posyniak <oleg.posyniak@gmail.com>
2016-11-09 23:01:33 +0200

    Initial

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ShiftedReality\MageConfDemo\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+    /**
+     * Renders page
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getConfig()->getTitle()->prepend(__('UI Components Demo'));
+
+        return $resultPage;
+    }
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ShiftedReality_MageConfDemo',
+    __DIR__
+);
