51805f347e59380a63ea123b9c840c4c182eb808
Oleh Posyniak <oposyniak@magento.com>
2016-11-22 18:54:55 +0200

    Added backend configuration

--- Ui/DemoDataProvider.php
+++ Ui/DemoDataProvider.php
@@ -2,9 +2,38 @@
 
 namespace ShiftedReality\MageConfDemo\Ui;
 
-use Magento\Catalog\Ui\DataProvider\Product\ProductDataProvider;
+use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider;
+use Magento\Ui\Component\Form\Element\Checkbox;
+use Magento\Ui\Component\Form\Element\DataType\Boolean;
+use Magento\Ui\Component\Form\Element\DataType\Text;
+use Magento\Ui\Component\Form\Element\Wysiwyg;
+use Magento\Ui\Component\Form\Field;
 
-class DemoDataProvider extends ProductDataProvider
+class DemoDataProvider extends DataProvider
 {
+    /**
+     * @inheritdoc
+     */
+    public function getMeta()
+    {
+        $meta = parent::getMeta();
 
+        $meta['general']['children']['dynamic']['arguments']['data']['config'] = [
+            'dataType' => Boolean::NAME,
+            'formElement' => Checkbox::NAME,
+            'notice' => __('This is dynamic element added from backend'),
+            'label' => __('Sample Checkbox'),
+            'code' => 'checkbox',
+            'source' => 'general',
+            'sortOrder' => 40,
+            'componentType' => Field::NAME,
+            'prefer' => 'toggle',
+            'valueMap' => [
+                'true' => '1',
+                'false' => '0',
+            ],
+        ];
+
+        return $meta;
+    }
 }
